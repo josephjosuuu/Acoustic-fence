dbf33c94f20eac548281625859ec90f4de5b2d59
Joseph John <49588749+Appachan10@users.noreply.github.com>
2021-05-29 22:44:35 +0530

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -13,7 +13,7 @@ time.sleep(1)
 port.write(b”AT+CMGF=1\r”)
 time.sleep(3)
 port.write(b’AT+CMGS=”6235819994″\r’)
-msg = “Alert..Wild animal detected”
+msg = “Alert..Wild animal detected” #This message send to farmer's phone
 print(“sending message….”)
 time.sleep(3)
 port.reset_output_buffer()
